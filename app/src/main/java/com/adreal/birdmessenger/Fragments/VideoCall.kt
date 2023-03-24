dec7dac5fc3819643dd210d7591bc440c43b3d3e
Aditya <adisingh925@gmail.com>
2023-03-24 08:17:46 +0530

    removed native webrtc implementation

--- app/src/main/java/com/adreal/birdmessenger/Fragments/VideoCall.kt
+++ app/src/main/java/com/adreal/birdmessenger/Fragments/VideoCall.kt
@@ -3,18 +3,30 @@ package com.adreal.birdmessenger.Fragments
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.SurfaceView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.adreal.birdmessenger.VideoCall.*
+import com.adreal.birdmessenger.R
 import com.adreal.birdmessenger.ViewModel.VideoCallViewModel
 import com.adreal.birdmessenger.databinding.FragmentVideoCallBinding
-import kotlinx.coroutines.*
+import io.agora.rtc.IRtcEngineEventHandler
+import io.agora.rtc.RtcEngine
+import io.agora.rtc.RtcEngineConfig
+import io.agora.rtc.models.ChannelMediaOptions
+import io.agora.rtc.video.VideoCanvas
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class VideoCall : Fragment() {
 
@@ -38,16 +50,38 @@ class VideoCall : Fragment() {
     lateinit var receiverToken: String
     lateinit var receiverName: String
 
+    private val appId = "489ab3de83944e87b7ffd278c4ccf35b"
+    private val channelName = "birdMessenger"
+    private val token = "007eJxTYEg65bDRd0tn6sHJ6uUdjgxPc1o0OzLntq3wcH1npsL64ZsCg4mFZWKScUqqhbGliUmqhXmSeVpaipG5RbJJcnKasWlS6xyZlIZARoZqD0FGRgYIBPF5GZIyi1J8U4uLU/PSU4sYGABUuyIy"
+    private val uid = 0
+    private var isJoined = false
+    private var agoraEngine: RtcEngine? = null
+    private var localSurfaceView: SurfaceView? = null
+    private var remoteSurfaceView: SurfaceView? = null
+
+    private var isAudioOn = true
+    private var isVideoOn = true
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         initValues()
-        requestCameraAndAudioPermission()
+
+        if (context?.let { checkSelfPermission(it,CAMERA_PERMISSION) }
+            == PackageManager.PERMISSION_GRANTED && context?.let { checkSelfPermission(it,CAMERA_PERMISSION) }
+            == PackageManager.PERMISSION_GRANTED) {
+            showMessage("all permission granted")
+            setupVideoSDKEngine()
+            joinChannel()
+
+        }else{
+            requestCameraAndAudioPermission()
+        }
 
         binding.switchCameraButton.setOnClickListener {
-
+            agoraEngine?.switchCamera()
         }
 
         videoCallViewModel.readAllCalls.observe(viewLifecycleOwner) {
@@ -61,15 +95,31 @@ class VideoCall : Fragment() {
         }
 
         binding.videoButton.setOnClickListener {
-
+            isVideoOn = if(isVideoOn){
+                agoraEngine?.muteLocalVideoStream(true)
+                binding.videoButton.setImageResource(R.drawable.video_off)
+                false
+            }else{
+                agoraEngine?.muteLocalVideoStream(false)
+                binding.videoButton.setImageResource(R.drawable.video)
+                true
+            }
         }
 
         binding.micButton.setOnClickListener {
-
+            isAudioOn = if(isAudioOn){
+                agoraEngine?.muteLocalAudioStream(true)
+                binding.micButton.setImageResource(R.drawable.mic_off)
+                false
+            }else{
+                agoraEngine?.muteLocalAudioStream(false)
+                binding.micButton.setImageResource(R.drawable.mic)
+                true
+            }
         }
 
         binding.endCallButton.setOnClickListener {
-
+            leaveChannel()
             findNavController().popBackStack()
         }
 
@@ -120,7 +170,7 @@ class VideoCall : Fragment() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == CAMERA_AUDIO_PERMISSION_REQUEST_CODE && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-//            onCameraAndAudioPermissionGranted()
+            onCameraAndAudioPermissionGranted()
         } else {
             onCameraPermissionDenied()
         }
@@ -128,5 +178,122 @@ class VideoCall : Fragment() {
 
     private fun onCameraPermissionDenied() {
         Toast.makeText(context, "Camera and Audio Permission Denied", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onCameraAndAudioPermissionGranted(){
+        setupVideoSDKEngine()
+    }
+
+    private fun setupVideoSDKEngine() {
+        try {
+            val config = RtcEngineConfig()
+            config.mContext = context
+            config.mAppId = appId
+            config.mEventHandler = mRtcEventHandler
+            agoraEngine = RtcEngine.create(config)
+            agoraEngine!!.enableVideo()
+        } catch (e: Exception) {
+            showMessage(e.toString())
+        }
+    }
+
+    private val mRtcEventHandler: IRtcEngineEventHandler = object : IRtcEngineEventHandler() {
+        // Listen for the remote host joining the channel to get the uid of the host.
+        override fun onUserJoined(uid: Int, elapsed: Int) {
+            showMessage("Remote user joined $uid")
+
+            // Set the remote video view
+            CoroutineScope(Dispatchers.Main).launch {
+                setupRemoteVideo(uid)
+            }
+        }
+
+        override fun onJoinChannelSuccess(channel: String, uid: Int, elapsed: Int) {
+            isJoined = true
+            showMessage("Joined Channel $channel")
+        }
+
+        override fun onUserOffline(uid: Int, reason: Int) {
+            showMessage("Remote user offline $uid $reason")
+
+            CoroutineScope(Dispatchers.Main).launch {
+                remoteSurfaceView!!.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setupRemoteVideo(uid: Int) {
+        val container: FrameLayout = binding.remoteView
+        remoteSurfaceView = SurfaceView(context)
+        remoteSurfaceView!!.setZOrderMediaOverlay(true)
+        container.addView(remoteSurfaceView)
+        agoraEngine!!.setupRemoteVideo(
+            VideoCanvas(
+                remoteSurfaceView,
+                VideoCanvas.RENDER_MODE_FIT,
+                uid
+            )
+        )
+        // Display RemoteSurfaceView.
+        remoteSurfaceView!!.visibility = View.VISIBLE
+    }
+
+    private fun setupLocalVideo() {
+        val container: FrameLayout = binding.localView
+        // Create a SurfaceView object and add it as a child to the FrameLayout.
+        localSurfaceView = SurfaceView(context)
+        container.addView(localSurfaceView)
+        // Call setupLocalVideo with a VideoCanvas having uid set to 0.
+        agoraEngine!!.setupLocalVideo(
+            VideoCanvas(
+                localSurfaceView,
+                VideoCanvas.RENDER_MODE_HIDDEN,
+                0
+            )
+        )
+    }
+
+    private fun joinChannel() {
+        val options = ChannelMediaOptions()
+        // For a Video call, set the channel profile as COMMUNICATION.
+//        options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION
+        // Set the client role as BROADCASTER or AUDIENCE according to the scenario.
+//        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER
+        // Display LocalSurfaceView.
+        setupLocalVideo()
+        localSurfaceView!!.visibility = View.VISIBLE
+        // Start local preview.
+        agoraEngine!!.startPreview()
+        // Join the channel with a temp token.
+        // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+        agoraEngine!!.joinChannel(token, channelName,"hi", uid, options)
+    }
+
+    private fun leaveChannel() {
+        if (!isJoined) {
+            showMessage("Join a channel first")
+        } else {
+            agoraEngine!!.leaveChannel()
+            showMessage("You left the channel")
+            // Stop remote video rendering.
+            if (remoteSurfaceView != null) remoteSurfaceView!!.visibility = View.GONE
+            // Stop local video rendering.
+            if (localSurfaceView != null) localSurfaceView!!.visibility = View.GONE
+            isJoined = false
+        }
+    }
+
+    fun showMessage(msg : String){
+        Log.d("Video Call",msg)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        agoraEngine!!.stopPreview()
+        agoraEngine!!.leaveChannel()
+        CoroutineScope(Dispatchers.IO).launch {
+            RtcEngine.destroy()
+            agoraEngine = null
+        }
     }
 }
